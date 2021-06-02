e7d8dcc67b7b1e67118c84c93bc0dee0c615a08e
dirkremie <remiedirk@gmail.com>
2021-06-02 13:43:07 +0200

    motordriver change

--- motordriver.py
+++ motordriver.py
@@ -1,21 +1,28 @@
 import board 
 import time 
 from digitalio import DigitalInOut, Direction, Pull
+import rotaryio
+from pwmio import PWMOut
+import math
 
 class MD13S:
 
-    def __init__(self, PWM_pin, dir_pin, feedback_pin):
+    def __init__(self, PWM_pin, dir_pin, enc_pin_A, enc_pin_B):
 
         self.PWMpin = PWM_pin
         self.DirPin = dir_pin
-        self._feedback = DigitalInOut(feedback_pin)
+        self.encPinA = enc_pin_A
+        self.encPinB = enc_pin_B
+        self._feedback = rotaryio.IncrementalEncoder(enc_pin_A, enc_pin_B)
+        self._pwm = PWMOut(PWM_pin, frequency=5000, duty_cycle=0)
     
 
     def speed_control(self, speedvector):
-
-
+        self._pwm.duty_cycle = 65535
+        
 
     def get_motor_speed(self):
+        motorspeed = self._feedback.value
 
 
        
