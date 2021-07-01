14a8110b26b908f5016fcde84a5222b4ffc921c9
Quirina300 <70889288+Quirina300@users.noreply.github.com>
2021-07-01 12:57:06 +0200

    Direction for motor added

--- motordriver.py
+++ motordriver.py
@@ -9,20 +9,32 @@ class MD13S:
 # Encoder resolution: 48 counts per motor shaft revolution
 # Gear ratio: 30:1
 
-    def __init__(self, PWM_pin, dir_pin, enc_pin_A, enc_pin_B):
+    def __init__(self, PWM_pin, dir_pin):
 
-        self.PWMpin = PWM_pin
-        self.DirPin = dir_pin
-        self.encPinA = enc_pin_A
-        self.encPinB = enc_pin_B
-        self._feedback = rotaryio.IncrementalEncoder(enc_pin_A, enc_pin_B)
-        self._pwm = PWMOut(PWM_pin, frequency=5000, duty_cycle=0)
+        self._PWMpin = PWM_pin
+        self._DirPin = DigitalInOut(dir_pin)
+        self._DirPin.direction = Direction.OUTPUT
+        # self._encPinA = enc_pin_A
+        # self._encPinB = enc_pin_B
+        # self._motorSP = motor_setpoint
+        # self._feedback = rotaryio.IncrementalEncoder(enc_pin_A, enc_pin_B)
+        self._pwm = PWMOut(PWM_pin)
+        self.last_position = None
     
 
     def speed_control(self, speedvector):
-        self._pwm.duty_cycle
-        
+        self._pwm.duty_cycle = speedvector
 
+    def direction_control(self,direction):
+        self._DirPin.value = direction
+        
     def get_motor_speed(self):
-        speed = self._feedback.position/48 # omw
-        return speed
+        position = self._feedback.position
+        if self.last_position == None:
+            print(1, position)
+
+        if position != self.last_position:
+            print(2, position)
+
+        self.last_position = position
+        print(3, position)
